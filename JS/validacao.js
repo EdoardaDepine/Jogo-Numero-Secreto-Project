796b9f8dcc45ff0eae7c94a811cd634174bc43a3
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-03 14:53:55 -0300

    criando dicas no jogo para o usuário

--- JS/validacao.js
+++ JS/validacao.js
@@ -1,19 +1,24 @@
 function verificaChutePossuiValorValido(chute) {
-  const numero = parseInt(chute);
+  const numero = +chute;
 
   if (Number.isNaN(numero)) {
     elementoChute.innerHTML += "<div>Valor inválido!</div>";
+    return;
   }
 
   if (numero < menorValor || numero > maiorValor) {
-    elementoChute.innerHTML +=
-      "<div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>";
+    elementoChute.innerHTML += `<div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    return;
   }
 
   if (numero === numeroSecreto) {
     document.body.innerHTML = `
     <h2>Você acertou!</h2>
-    <h3>O número secreto era ${numeroSecreto}!
+    <h3>O número secreto era ${numeroSecreto}!</h3>
     `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>`;
+  } else {
+    elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>`;
   }
 }
